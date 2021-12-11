22276565e1056cb275efd52a9cf43c9fb16244da
Nadine Sawotin <nadines@pandell.com>
2021-12-10 23:30:39 -0700

    Day 10

--- days/day10.test.ts
+++ days/day10.test.ts
@@ -4,10 +4,10 @@ const inputPath = "./Inputs/day10Test.txt"
 
 it("calculates part 1 correctly", () => {
     const answer = part1(inputPath);
-    expect (answer).toBe(0);
+    expect (answer).toBe(26397);
 })
 
 it("calculates part 2 correctly", () => {
     const answer = part2(inputPath);
-    expect (answer).toBe(0);
+    expect (answer).toBe(288957);
 })
--- days/day10.ts
+++ days/day10.ts
@@ -1,9 +1,101 @@
 import fs from 'fs';
 
 export function part1(path: string): number {
-   return 0;
+    const array = fs.readFileSync(path).toString().split('\n');
+    let points = 0;
+
+    // process the "corrupt" lines
+    for (let line of array) {
+        let openChars: string[] = [];
+        points += ProcessLine(line, openChars, false);
+    }
+    
+    return points;
 }
 
 export function part2(path: string): number {
-    return 0;
+    const array = fs.readFileSync(path).toString().split('\n');
+    let pointsArr: number[] = [];
+    
+    // process the "incomplete" lines
+    for (let line of array) {
+        let openChars: string[] = [];
+        pointsArr.push(ProcessLine(line, openChars, true));
+    }
+    
+    pointsArr = pointsArr.filter(a => a > 0).sort((a, b) => a - b);
+    return pointsArr[Math.floor(pointsArr.length / 2)];
+}
+
+function ProcessLine(line: string, openChars: string[], part2: boolean): number {
+    let points = 0;
+    let incompletePoints = 0;
+    let lineCorrupt = false;
+
+    for (var i = 0; i < line.length; i++) {
+        let char = line.charAt(i);
+
+        if (['(', '[', '{', '<'].indexOf(char) >= 0) {
+            // open char
+            openChars.push(char);
+        } else if ([')', ']', '}', '>'].indexOf(char) >= 0) {
+            // closing char
+            switch(char) { 
+                case ')': {
+                    if(openChars[openChars.length-1] !== '(') {
+                        points +=3;
+                        lineCorrupt = true;
+                    }
+                    openChars.pop();
+                    break;
+                }
+                case ']':
+                    if(openChars[openChars.length-1] !== '[') {
+                        points +=57;
+                        lineCorrupt = true;
+                    }
+                    openChars.pop();
+                    break;
+                case '}':
+                    if(openChars[openChars.length-1] !== '{') {
+                        points +=1197;
+                        lineCorrupt = true;
+                    }
+                    openChars.pop();
+                    break;
+                case '>':
+                    if(openChars[openChars.length-1] !== '<') {
+                        points +=25137;
+                        lineCorrupt = true;
+                    }
+                    openChars.pop();
+                    break;
+            }
+        }
+    }
+
+    if(part2 && !lineCorrupt) {
+        const missingChars = openChars.reverse();
+        for (let openChar of missingChars) { 
+            switch(openChar) { 
+                case '(':
+                    incompletePoints = incompletePoints*5 + 1;
+                    break;
+                case '[':
+                    incompletePoints = incompletePoints*5 + 2;
+                    break;
+                case '{':
+                    incompletePoints = incompletePoints*5 + 3;
+                    break;
+                case '<':
+                    incompletePoints = incompletePoints*5 + 4;
+                    break;
+            }
+        }
+        return incompletePoints;
+    } else if (part2 && lineCorrupt) {
+        return 0;
+    }
+
+    return points;
 }
